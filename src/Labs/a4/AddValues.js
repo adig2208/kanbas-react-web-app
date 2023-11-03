b15447aa92616c8c16ee5ade1a41d86b4a4fdc30
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-02 21:39:34 -0400

    Update AddValues.js

--- src/Labs/a4/AddValues.js
+++ src/Labs/a4/AddValues.js
@@ -1,13 +1,12 @@
 function AddValues({ a, b }) {
-    return ( <
-        div >
-        <
-        h2 > Add < /h2>  <
-        p >
-
-        { a } + { b } = { a + b } <
-        /p>  <
-        /div>
-    );
+  return (
+    <div>
+      <h2> Add </h2> 
+      <p>
+         
+        {a} + {b} = {a + b} 
+      </p> 
+    </div>
+  );
 }
 export default AddValues;
