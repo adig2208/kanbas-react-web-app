719d5d322e2c7a31acb6dae3b3a62f13f712d952
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-02 21:35:43 -0400

    Update index.js

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -8,65 +8,56 @@ import { Provider } from "react-redux"; // Import Provider
 import store from "./store"; // Import the store
 
 function Kanbas() {
-    const [courses, setCourses] = useState(db.courses);
-    const [course, setCourse] = useState({
-        name: "",
-        number: "",
-        startDate: "",
-        endDate: "",
-    });
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    name: "",
+    number: "",
+    startDate: "",
+    endDate: "",
+  });
 
-    const addNewCourse = () => {
-        setCourses([...courses, {...course, _id: new Date().getTime() }]);
-    };
+  const addNewCourse = () => {
+    setCourses([...courses, { ...course, _id: new Date().getTime() }]);
+  };
 
-    const deleteCourse = (courseId) => {
-        setCourses(courses.filter(c => c._id !== courseId));
-    };
+  const deleteCourse = (courseId) => {
+    setCourses(courses.filter((c) => c._id !== courseId));
+  };
 
-    const updateCourse = (courseId, name) => {
-        setCourses(courses.map(c => (c._id === courseId ? {...c, name } : c)));
-    };
+  const updateCourse = (courseId, name) => {
+    setCourses(courses.map((c) => (c._id === courseId ? { ...c, name } : c)));
+  };
 
-    return ( <
-            Provider store = { store } >
-            <
-            div className = "d-flex" >
-            <
-            KanbasNavigation / >
-            <
-            div style = {
-                { display: 'contents' } } >
-            <
-            Routes >
-            <
-            Route path = "/"
-            element = { < Navigate to = "Dashboard" / > }
-            /> <
-            Route path = "Account"
-            element = { < h1 > Account < /h1>} / >
-                <
-                Route path = "Dashboard"
-                element = { <
-                    Dashboard
-                    courses = { courses }
-                    course = { course }
-                    setCourse = { setCourse }
-                    addNewCourse = { addNewCourse }
-                    deleteCourse = { deleteCourse }
-                    updateCourse = { updateCourse }
-                    />
-                }
-                /> <
-                Route path = "Courses/:courseId/*"
-                element = { < Courses courses = { courses }
-                    />} / >
-                    <
-                    /Routes> <
-                    /div> <
-                    /div> <
-                    /Provider>
-                );
-            }
+  return (
+    <Provider store={store}>
+      <div className="d-flex">
+        <KanbasNavigation />
+        <div style={{ display: "contents" }}>
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} /> 
+            <Route path="Account" element={<h1> Account </h1>} />
+            <Route
+              path="Dashboard"
+              element={
+                <Dashboard
+                  courses={courses}
+                  course={course}
+                  setCourse={setCourse}
+                  addNewCourse={addNewCourse}
+                  deleteCourse={deleteCourse}
+                  updateCourse={updateCourse}
+                />
+              }
+            /> 
+            <Route
+              path="Courses/:courseId/*"
+              element={<Courses courses={courses} />}
+            />
+          </Routes> 
+        </div> 
+      </div> 
+    </Provider>
+  );
+}
 
-            export default Kanbas;
+export default Kanbas;
