7f499075d8e3e614c382b687a783cf8680d4c59a
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-02 21:43:42 -0400

    Update index.js

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -13,36 +13,23 @@ import ParentStateComponent from "./ParentStateComponent";
 import ReduxExamples from "./ReduxExamples";
 
 function Assignment4() {
-    function sayHello() {
-        alert("Hello");
-    }
-    return ( <
-        div >
-        <
-        h1 > Assignment 4 < /h1> <AddValues a={1} b={2} / > < ClickEvent / >
-        <
-        PassingDataOnEvent / >
-        <
-        PassingFunctions theFunction = { sayHello }
-        /> <EventObject / >
-        <
-        Counter / >
-        <
-        BooleanStateVariables / >
-        <
-        StringStateVariables / >
-        <
-        DateStateVariable / >
-        <
-        ObjectStateVariable / >
-        <
-        ArrayStateVariable / >
-        <
-        ParentStateComponent / >
-        <
-        ReduxExamples / >
-        <
-        /div>
-    );
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div>
+      <h1> Assignment 4 </h1> <AddValues a={1} b={2} /> <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} /> <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples />
+    </div>
+  );
 }
 export default Assignment4;
