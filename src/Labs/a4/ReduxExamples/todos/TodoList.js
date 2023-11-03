3fd743e8ead07ae0f5044d4b93c1fc879851ebda
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-02 21:38:42 -0400

    Update TodoList.js

--- src/Labs/a4/ReduxExamples/todos/TodoList.js
+++ src/Labs/a4/ReduxExamples/todos/TodoList.js
@@ -4,21 +4,17 @@ import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
 
 function TodoList() {
-    const { todos } = useSelector((state) => state.todosReducer);
-    return ( <
-        div >
-        <
-        h2 > Todo List < /h2>  <
-        ul className = "list-group" >
-        <
-        TodoForm / > {
-            todos.map((todo) => ( <
-                TodoItem todo = { todo }
-                />
-            ))
-        } <
-        /ul>  <
-        /div>
-    );
+  const { todos } = useSelector((state) => state.todosReducer);
+  return (
+    <div>
+      <h2> Todo List </h2> 
+      <ul className="list-group">
+        <TodoForm /> 
+        {todos.map((todo) => (
+          <TodoItem todo={todo} />
+        ))} 
+      </ul> 
+    </div>
+  );
 }
 export default TodoList;
