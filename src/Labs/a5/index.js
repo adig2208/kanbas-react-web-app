0295ef957e02aba3ac1920dfa3d5c5907c242650
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-21 00:09:15 -0500

    Update index.js

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -1,21 +1,22 @@
 import EncodingParametersInURLs from "./EncodingParametersInURLs";
-import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
-const API_LAB_BASE = process.env.REACT_APP_API_LAB_BASE;
-
+import WorkingWithObjects from "./WorkingWithObjects";
+const hostedServer = process.env.REACT_APP_LAB_BASE;
 function Assignment5() {
-  return (
-    <div>
-      <h1> Assignment 5 </h1>
-      <div className="list-group">
-        <a href={`${API_LAB_BASE}/a5/welcome`} className="list-group-item">
-          Welcome
-        </a>
-      </div>
-      <EncodingParametersInURLs />
-      <WorkingWithObjects />
-      <WorkingWithArrays />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Assignment 5</h1>
+            <div className="list-group">
+                <a href={`${hostedServer}/a5/welcome`}
+                    className="list-group-item">
+                    Welcome
+                </a>
+            </div>
+            <EncodingParametersInURLs />
+            <WorkingWithObjects />
+            <WorkingWithArrays />
+        </div>
+    );
 }
 export default Assignment5;
+
