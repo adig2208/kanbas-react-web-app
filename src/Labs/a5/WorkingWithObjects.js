3619dbd5a9975f0274beb9578fd117c08f526605
Aditya Gurnani <adityagurnani@gmail.com>
2023-11-21 00:08:18 -0500

    Update WorkingWithObjects.js

--- src/Labs/a5/WorkingWithObjects.js
+++ src/Labs/a5/WorkingWithObjects.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function WorkingWithObjects() {
-    const hostedServer = process.env.REACT_APP_HOST_SERVER;
+    const hostedServer = process.env.REACT_APP_LAB_BASE;
     const [assignment, setAssignment] = useState({
         id: 1,
         title: "NodeJS Assignment",
