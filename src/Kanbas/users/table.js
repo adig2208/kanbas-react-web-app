25247ec4352767049570aaf69ed67f84d3ac91c1
Aditya Gurnani <adityagurnani@gmail.com>
2023-12-01 22:37:37 -0500

    Update table.js

--- src/Kanbas/users/table.js
+++ src/Kanbas/users/table.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {Link, useNavigate, useParams  } from "react-router-dom";
 import {
     BsTrash3Fill, BsFillCheckCircleFill,
     BsPencil, BsPlusCircleFill,
