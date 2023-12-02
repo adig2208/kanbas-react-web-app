bf6b60bbf853d56cc5ab150986116ab045c06e75
Aditya Gurnani <adityagurnani@gmail.com>
2023-12-02 16:42:13 -0500

    Update signup.js

--- src/Kanbas/users/signup.js
+++ src/Kanbas/users/signup.js
@@ -15,7 +15,7 @@ function Signup() {
     }
   };
   return (
-    <div style={{ marginLeft: '250px' }}>
+    <div style={{ marginLeft: '200px' }}>
       <h1>Signup</h1>
       {error && <div>{error}</div>}
       <input
