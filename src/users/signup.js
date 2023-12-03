d08733a747f6f4e3dfdcb15806ff610753a885e1
Aditya Gurnani <adityagurnani@gmail.com>
2023-12-03 15:38:50 -0500

    Update signup.js

--- src/users/signup.js
+++ src/users/signup.js
@@ -2,36 +2,57 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as client from "./client";
 function Signup() {
-  const [error, setError] = useState("");
-  const [credentials, setCredentials] = useState({
-    username: "", password: "" });
-  const navigate = useNavigate();
-  const signup = async () => {
-    try {
-      await client.signup(credentials);
-      navigate("/project/account");
-    } catch (err) {
-      setError(err.response.data.message);
-    }
-  };
-  return (
-    <div style={{ marginLeft: '200px' }}>
-      <h1>Signup</h1>
-      {error && <div>{error}</div>}
-      <input
-        value={credentials.username}
-        onChange={(e) => setCredentials({
-          ...credentials,
-          username: e.target.value })} />
-      <input
-        value={credentials.password}
-        onChange={(e) => setCredentials({
-          ...credentials,
-          password: e.target.value })} />
-      <button onClick={signup}>
-        Signup
-      </button>
-    </div>
-  );
+    const [error, setError] = useState("");
+    const [credentials, setCredentials] = useState({
+        username: "", password: ""
+    });
+    const navigate = useNavigate();
+    const signup = async () => {
+        try {
+            await client.signup(credentials);
+            navigate("/project/account");
+        } catch (err) {
+            setError(err.response.data.message);
+        }
+    };
+    return (
+        <div>
+            <h1>Signup</h1>
+            {error && <div>{error}</div>}
+            <form>
+                <div className="col-3">
+                    <label htmlFor="username" className="form-label">
+                        Username
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Enter username"
+                        value={credentials.username}
+                        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+                    />
+                </div>
+                <br />
+                <div className="col-3">
+                    <label htmlFor="password" className="form-label">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Enter password"
+                        value={credentials.password}
+                        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                    />
+                </div>
+                <br />
+                <button type="button" className="btn btn-primary" onClick={signup}>
+                    Signup
+                </button>
+            </form>
+        </div>
+    );
 }
 export default Signup;
