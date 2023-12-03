1ce5e903a327ff46c6dfc22460a811f3eb8162d5
Aditya Gurnani <adityagurnani@gmail.com>
2023-12-03 16:39:02 -0500

    Update client.js

--- src/users/client.js
+++ src/users/client.js
@@ -5,8 +5,8 @@ const request = axios.create({
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
-    const response = await request.post(`${USERS_API}/signin`, credentials);
     console.log(credentials);
+    const response = await request.post(`${USERS_API}/signin`, credentials);
     return response.data;
 };
 export const account = async () => {
