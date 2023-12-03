e53c1e378c6e1db6f2f61d62cced69ab39568832
Aditya Gurnani <adityagurnani@gmail.com>
2023-12-03 15:01:10 -0500

    Update account.js

--- src/Kanbas/users/account.js
+++ src/Kanbas/users/account.js
@@ -10,7 +10,7 @@ function Account() {
   };
   const signout = async () => {
     await client.signout();
-    navigate("/Kanbas/signin");
+    navigate("/project/signin");
   };
   const navigate = useNavigate();
   const fetchAccount = async () => {
@@ -62,7 +62,7 @@ function Account() {
   <button onClick={signout}>
     Signout
   </button>
-  <Link to="/kanbas/admin/users" className="btn btn-warning w-100">
+  <Link to="/project/admin/users" className="btn btn-warning w-100">
     Users
   </Link>
         </div>
