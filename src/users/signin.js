3ade9f70cd04c7eb1a3361ed160a3e4882b3e678
Aditya Gurnani <adityagurnani@gmail.com>
2023-12-03 15:38:28 -0500

    Update signin.js

--- src/users/signin.js
+++ src/users/signin.js
@@ -1,20 +1,54 @@
 import * as client from "./client";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 function Signin() {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
-  const navigate = useNavigate();
-  const signin = async () => {
-    await client.signin(credentials);
-    navigate("/project/account");
-  };
-  return (
-    <div style={{ marginLeft: '200px' }}>
-      <h1>Signin</h1>
-      <input value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})}/>
-      <input value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
-      <button onClick={signin}> Signin </button>
-    </div>
-  );
+    const [credentials, setCredentials] = useState({ username: "", password: "" });
+    const navigate = useNavigate();
+
+    const signin = async () => {
+        await client.signin(credentials);
+        navigate("/project/account");
+    };
+
+    return (
+        <div>
+            <h1>Signin</h1>
+            <form>
+                <div className="col-3">
+                    <label htmlFor="username" className="form-label">
+                        Username
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Enter your username"
+                        value={credentials.username}
+                        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+                    />
+                </div>
+                <br />
+                <div className="col-3">
+                    <label htmlFor="password" className="form-label">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Enter your password"
+                        value={credentials.password}
+                        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+                    />
+                </div>
+                <br />
+                <button type="button" className="btn btn-primary" onClick={signin}>
+                    Signin
+                </button>
+            </form>
+        </div>
+    );
 }
+
 export default Signin;
